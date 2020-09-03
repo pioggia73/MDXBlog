720676f8a291f3a5b03d01eb99b8c32210aeac0a
Monika Wasilewska <monikawasilewska13@wp.pl>
2020-09-03 20:28:26 +0200

    adding newsletter page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,7 +7,15 @@ const NewsLetter = () => {
               <div className='page-center'>
                 <h2>Get all the latest stories to your inbox</h2>
                 <h4>I write to my friends every few weeks</h4>
-                <form className='contact-form'>
+                <form className='contact-form'
+                      name='testing-contact'
+                      method='POST'
+                      netlify-honeypot='bot-field'
+                      data-netlify='true'
+                      action='/success'
+                >
+                  <input type='hidden' name='bot-field' />
+                  <input type='hidden' name='form-name' value='testing-contact' />
                   <input 
                     type='text' 
                     name='name' 
@@ -27,6 +35,6 @@ const NewsLetter = () => {
               </div>
             </section>
           </Layout>
-}
+};
 
 export default NewsLetter;
--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Link } from 'gatsby'
 const Success = () => {
-  return <h2>success page</h2>
-}
+  return  <Layout>
+            <section className='success-page'>
+              <div className='page-center'>
+                <h2>Your submission was received !</h2>
+                <Link to='/' className='btn'>back home</Link>
+              </div>
+            </section>
+          </Layout>
+};
 
-export default Success
+export default Success;
